e28564808782199842c6ac0c17b9901312819b7f
site.net <129742936+Armin3080@users.noreply.github.com>
2023-12-21 00:05:15 +0330

    Update java.js

--- java.js
+++ java.js
@@ -1,75 +1,58 @@
-const WebSocket = require('ws');
+const socket = new WebSocket('ws://github.com/Armin3080/webchat/'); // آدرس سرور WebSocket
 
-const wss = new WebSocket.Server({ port: 8080 });
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    // Broadcast received messages to all connected clients
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  });
-});
-
-const ws = new WebSocket('ws://localhost:8080'); // آدرس سرور WebSocket
-
-// برقراری اتصال به WebSocket
-ws.addEventListener('open', function open() {
+socket.addEventListener('open', function open() {
   console.log('Connected to the WebSocket server');
 });
 
-// دریافت پیام‌های جدید
-ws.addEventListener('message', function incoming(message) {
-  const chatMessages = document.querySelector('.chat-messages');
-  const messageElement = document.createElement('div');
-  messageElement.classList.add('message');
-  messageElement.textContent = message.data; // نمایش پیام در تگ div با کلاس message
-  chatMessages.appendChild(messageElement);
-  chatMessages.scrollTop = chatMessages.scrollHeight;
+socket.addEventListener('message', function incoming(message) {
+  console.log('Received message: ', message.data);
 });
 
-function sendMessage() {
-  const messageInput = document.getElementById('message-input');
-  const message = messageInput.value.trim();
-  const username = messageInput.getAttribute('data-username');
-
-  if (message !== '' && username) {
-    const fullMessage = username + ': ' + message;
-    ws.send(fullMessage); // ارسال پیام به سرور WebSocket
-    messageInput.value = '';
-  }
+function sendMessage(message) {
+  socket.send(message);
 }
-
-document.getElementById('send-button').addEventListener('click', sendMessage);
-document.getElementById('message-input').addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    event.preventDefault();
-    sendMessage();
-  }
-});
-
-// این بخش از کد مشابه قسمت اول اسکریپت شماست که نام کاربری را دریافت و به ویژگی data-username اضافه می‌کند
 document.getElementById('username-button').addEventListener('click', function () {
-  const usernameInput = document.getElementById('username-input');
-  const messageInput = document.getElementById('message-input');
-  const sendButton = document.getElementById('send-button');
+    const usernameInput = document.getElementById('username-input');
+    const messageInput = document.getElementById('message-input');
+    const sendButton = document.getElementById('send-button');
 
-  const username = usernameInput.value.trim();
-  if (username !== '') {
-    // Enable message input and send button
-    messageInput.removeAttribute('disabled');
-    sendButton.removeAttribute('disabled');
+    const username = usernameInput.value.trim();
+    if (username !== '') {
+      // Enable message input and send button
+      messageInput.removeAttribute('disabled');
+      sendButton.removeAttribute('disabled');
 
-    // Hide username input and button
-    usernameInput.style.display = 'none';
-    this.style.display = 'none';
+      // Hide username input and button
+      usernameInput.style.display = 'none';
+      this.style.display = 'none';
 
-    // Save username in a data attribute
-    messageInput.setAttribute('data-username', username);
-    messageInput.focus();
+      // Save username in a data attribute
+      messageInput.setAttribute('data-username', username);
+      messageInput.focus();
+    }
+  });
+
+  function sendMessage() {
+    const messageInput = document.getElementById('message-input');
+    const message = messageInput.value.trim();
+    const username = messageInput.getAttribute('data-username');
+
+    if (message !== '' && username) {
+      const chatMessages = document.querySelector('.chat-messages');
+      const messageElement = document.createElement('div');
+      messageElement.classList.add('message');
+      messageElement.textContent = username + ': ' + message; // Show username with message
+      chatMessages.appendChild(messageElement);
+      messageInput.value = '';
+      chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
   }
-});
 
+  document.getElementById('send-button').addEventListener('click', sendMessage);
+  document.getElementById('message-input').addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      sendMessage();
+    }
+  });
 

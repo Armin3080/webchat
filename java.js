e0028232343c745f7db9bc01195b4fbfe9d3c45a
site.net <129742936+Armin3080@users.noreply.github.com>
2023-12-20 23:50:07 +0330

    Update java.js

--- java.js
+++ java.js
@@ -1,46 +1,75 @@
-document.getElementById('username-button').addEventListener('click', function () {
-    const usernameInput = document.getElementById('username-input');
-    const messageInput = document.getElementById('message-input');
-    const sendButton = document.getElementById('send-button');
+const WebSocket = require('ws');
 
-    const username = usernameInput.value.trim();
-    if (username !== '') {
-      // Enable message input and send button
-      messageInput.removeAttribute('disabled');
-      sendButton.removeAttribute('disabled');
+const wss = new WebSocket.Server({ port: 11754 });
 
-      // Hide username input and button
-      usernameInput.style.display = 'none';
-      this.style.display = 'none';
-
-      // Save username in a data attribute
-      messageInput.setAttribute('data-username', username);
-      messageInput.focus();
-    }
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    // Broadcast received messages to all connected clients
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
   });
+});
 
-  function sendMessage() {
-    const messageInput = document.getElementById('message-input');
-    const message = messageInput.value.trim();
-    const username = messageInput.getAttribute('data-username');
+const ws = new WebSocket('tcp://6.tcp.ngrok.io:11754'); // آدرس سرور WebSocket
 
-    if (message !== '' && username) {
-      const chatMessages = document.querySelector('.chat-messages');
-      const messageElement = document.createElement('div');
-      messageElement.classList.add('message');
-      messageElement.textContent = username + ': ' + message; // Show username with message
-      chatMessages.appendChild(messageElement);
-      messageInput.value = '';
-      chatMessages.scrollTop = chatMessages.scrollHeight;
-    }
+// برقراری اتصال به WebSocket
+ws.addEventListener('open', function open() {
+  console.log('Connected to the WebSocket server');
+});
+
+// دریافت پیام‌های جدید
+ws.addEventListener('message', function incoming(message) {
+  const chatMessages = document.querySelector('.chat-messages');
+  const messageElement = document.createElement('div');
+  messageElement.classList.add('message');
+  messageElement.textContent = message.data; // نمایش پیام در تگ div با کلاس message
+  chatMessages.appendChild(messageElement);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+function sendMessage() {
+  const messageInput = document.getElementById('message-input');
+  const message = messageInput.value.trim();
+  const username = messageInput.getAttribute('data-username');
+
+  if (message !== '' && username) {
+    const fullMessage = username + ': ' + message;
+    ws.send(fullMessage); // ارسال پیام به سرور WebSocket
+    messageInput.value = '';
   }
+}
 
-  document.getElementById('send-button').addEventListener('click', sendMessage);
-  document.getElementById('message-input').addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      sendMessage();
-    }
-  });
+document.getElementById('send-button').addEventListener('click', sendMessage);
+document.getElementById('message-input').addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    sendMessage();
+  }
+});
+
+// این بخش از کد مشابه قسمت اول اسکریپت شماست که نام کاربری را دریافت و به ویژگی data-username اضافه می‌کند
+document.getElementById('username-button').addEventListener('click', function () {
+  const usernameInput = document.getElementById('username-input');
+  const messageInput = document.getElementById('message-input');
+  const sendButton = document.getElementById('send-button');
+
+  const username = usernameInput.value.trim();
+  if (username !== '') {
+    // Enable message input and send button
+    messageInput.removeAttribute('disabled');
+    sendButton.removeAttribute('disabled');
+
+    // Hide username input and button
+    usernameInput.style.display = 'none';
+    this.style.display = 'none';
+
+    // Save username in a data attribute
+    messageInput.setAttribute('data-username', username);
+    messageInput.focus();
+  }
+});
 
 
